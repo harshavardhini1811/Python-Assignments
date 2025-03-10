2a9735d43e1251da0eb41e0d8b7beec9e5797178
harshavardhini1811 <karriharshavardhini0@gmail.com>
2025-03-10 17:09:35 +0530

    Add files via upload

---
+++ inc&dec.py
@@ -0,0 +1,11 @@
+def increment_decrement():
+    x = 30
+    print("initial value:", x)
+
+    x += 2
+    print("after increment:", x)
+
+    x -=2
+    print("after decrement:", x)
+
+increment_decrement()    
